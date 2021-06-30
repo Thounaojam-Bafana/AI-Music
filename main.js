4af142b65af07ee1ee4d3c6e49a02d68066e7abd
Thounaojam-Bafana <76650032+Thounaojam-Bafana@users.noreply.github.com>
2021-06-30 13:40:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 var chidi_blauri,ukhidari_nangumba;
+var leftWristX = 0;
+var leftWristY = 0;
+var rightWristX = 0;
+var rightWristY = 0;
 
 function preload(){
     ukhidari_nangumba = loadSound('Ukhidari Nangumba Shilheiba & Biju 2020 Gee Thoibi Movie Official Song Release.mp3');
@@ -10,8 +14,24 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
 
 function draw(){
     image(video, 0, 0, 500, 500);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is Initialised!");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+    }
 }
